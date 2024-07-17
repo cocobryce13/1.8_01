2fa2db0cd4871406dafb2764bdb8b83d3480a6a7
sokol <sokol@ASUS>
2024-07-17 22:13:28 +0500

    Добавьте файлы проекта.

---
+++ 1.8_01.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <windows.h>
+
+void counting_function()
+{
+    static int static_var = 0;
+    static_var++;
+    std::cout << "Количество вызовов функции counting_function(): " << static_var << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    for (int i = 0; i < 15; i++)
+    {
+        counting_function();
+    }
+
+    return 0;
+}
